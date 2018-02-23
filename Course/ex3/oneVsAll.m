16dad6b9f751ec996eb3b0443ff23341286e7e16
Unknown <rpedrazacoello@gmail.com>
2018-02-23 11:45:08 -0700

    Comienzo ex3
    Empece los ex3, no los he terminado. 
    
    Tengo dudas de lo que he resuelto

---
+++ Course/ex3/oneVsAll.m
@@ -0,0 +1,90 @@
+function [all_theta] = oneVsAll(X, y, num_labels, lambda)
+%ONEVSALL trains multiple logistic regression classifiers and returns all
+%the classifiers in a matrix all_theta, where the i-th row of all_theta 
+%corresponds to the classifier for label i
+%   [all_theta] = ONEVSALL(X, y, num_labels, lambda) trains num_labels
+%   logistic regression classifiers and returns each of these classifiers
+%   in a matrix all_theta, where the i-th row of all_theta corresponds 
+%   to the classifier for label i
+
+% Some useful variables
+m = size(X, 1);
+n = size(X, 2);
+
+% You need to return the following variables correctly 
+all_theta = zeros(num_labels, n + 1);
+
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: You should complete the following code to train num_labels
+%               logistic regression classifiers with regularization
+%               parameter lambda. 
+%
+% Hint: theta(:) will return a column vector.
+%
+% Hint: You can use y == c to obtain a vector of 1's and 0's that tell you
+%       whether the ground truth is true/false for this class.
+%
+% Note: For this assignment, we recommend using fmincg to optimize the cost
+%       function. It is okay to use a for-loop (for c = 1:num_labels) to
+%       loop over the different classes.
+%
+%       fmincg works similarly to fminunc, but is more efficient when we
+%       are dealing with large number of parameters.
+%
+% Example Code for fmincg:
+%
+%     % Set Initial theta
+%     initial_theta = zeros(n + 1, 1);
+%     
+%     % Set options for fminunc
+%     options = optimset('GradObj', 'on', 'MaxIter', 50);
+% 
+%     % Run fmincg to obtain the optimal theta
+%     % This function will return theta and the cost 
+%     [theta] = ...
+%         fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
+%                 initial_theta, options);
+%
+
+options = optimset('GradObj', 'on', 'MaxIter', 50);
+
+for i=1:num_labels,
+
+	initial_theta = all_theta(i, :)';
+
+	%WHAT DOES FMINCG DO?
+	[all_theta(i, :)] = fmincg (@(t)(lrCostFunction(t, X, (y==i), lambda)), initial_theta, options);
+
+	% tempTheta = all_theta(i, :)';
+
+	% size(tempTheta)
+
+	% htheta = sigmoid(X * tempTheta); % size = (5 x 1)
+	% first = (-y .* log(htheta));
+	% second = (1 - y) .* log (1 - htheta);
+	% J = 1/m * sum(first - second) + (lambda/(2*m) * sum(tempTheta(2:end).^2)); %theta(1) i think it's not supposed to be touch, you're not suppose to regularize theta0
+
+	% temp = tempTheta;
+	% temp(1) = 0;
+	% error = htheta - y;
+
+	% intento = ((1 / m) * (X' * error)) + (lambda/m * temp); %You're not supposed to regularize theta0, that's why you use 
+	
+	% size(intento)
+
+end
+
+
+
+
+
+
+
+
+% =========================================================================
+
+
+end
